174d7982eb3fd48e637754c9df77943fe9f00fbf
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-06-30 23:03:06 +0900

    Fix script.

--- trigger_all.sh
+++ trigger_all.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-array=(
-  "ocaml:$OCAML_TRIGGER_TOKEN"
-  "debian:$DEBIAN_TRIGGER_TOKEN"
-  "coq:$COQ_TRIGGER_TOKEN"
-  "coqide:$COQIDE_TRIGGER_TOKEN"
-  "eliom:$ELIOM_TRIGGER_TOKEN"
+set -u
+
+declare -A arr=(
+  [ocaml]="$OCAML_TRIGGER_TOKEN"
+  [debian]="$DEBIAN_TRIGGER_TOKEN"
+  [coq]="$COQ_TRIGGER_TOKEN"
+  [coqide]="$COQIDE_TRIGGER_TOKEN"
+  [eliom]="$ELIOM_TRIGGER_TOKEN"
 )
 
-for index in "${array[@]}" ; do
-  name="${index##*::}"
-  token="${index%%::*}"
-  curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/tyabu/$name/trigger/$token/
+for key in ${!arr[*]} ; do
+  name=$key
+  token=${arr[$key]}
+  curl -H 'Content-Type: application/json' --data '{"build": true}' -X POST https://registry.hub.docker.com/u/tyabu/$name/trigger/$token/
 done
+
